1fa2efc2793ce3fb18830a22431a3f0e8214b8a5
PaolaDM <orchidea.preziosa@gmail.com>
2020-03-05 22:04:07 +0000

    Updates

---
+++ precache-manifest.9bebc9655fe1cb2c42a93297e283c584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e4b0ffc690853782d4cc400bd26884",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "f556e6a421c32f6b3b0f",
+    "url": "/testReact/static/css/main.0cd32a54.chunk.css"
+  },
+  {
+    "revision": "644992cf89e1fb95a8d6",
+    "url": "/testReact/static/js/2.f8384d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/testReact/static/js/2.f8384d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f556e6a421c32f6b3b0f",
+    "url": "/testReact/static/js/main.5f7b6ee8.chunk.js"
+  },
+  {
+    "revision": "fa670936539d38dfde10",
+    "url": "/testReact/static/js/runtime-main.0f9b87d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
